678576f22ba1e0249d3d687e64fa1dfe3515d3f9
LiiOr <laure.delamarre@hotmail.fr>
2021-11-23 06:56:06 +0000

    TP1  - Gestion des données avec doctrine / mise en place page articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class ArticleController extends AbstractController
      */
     public function index(): Response
     {
+        $articleRepository = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $articleRepository->findAll();
+        //dd($articles);
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles' => $articles
         ]);
     }
 }
