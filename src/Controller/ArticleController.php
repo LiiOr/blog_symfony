e6eda597655ca89580b70492ad98f1fc902cdd84
LiiOr <laure.delamarre@hotmail.fr>
2021-12-01 14:32:54 +0000

    ajout pagination et style bootstrap. Passer au TP2 pour prochaine session

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -12,10 +14,12 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article", name="article.index")
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
         $articleRepository = $this->getDoctrine()->getRepository(Article::class);
         $articles = $articleRepository->findAll();
+
+        $articles = $paginator->paginate($articles, $request->query->getInt('page', 1), 10);
         //dd($articles);
         return $this->render('article/index.html.twig', [
             //'controller_name' => 'ArticleController',
