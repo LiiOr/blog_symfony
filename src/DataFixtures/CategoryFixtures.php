7407d980038f8684c24a2b0eef2b27c1f8893c80
LiiOr <laure.delamarre@hotmail.fr>
2021-12-08 14:20:10 +0000

    TP2 stoppe sur fixtures categories

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+use Faker\Factory;
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+//use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class CategoryFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create();
+            
+        for ($i = 1; $i <= 10; $i++)
+        {
+            $category = new Category();
+    
+            $category->setName($faker->name())
+                    ->setDescription($faker->text(300));              
+            $manager->persist($category);
+        }
+    
+        $manager->flush();
+    }
+    
+}
